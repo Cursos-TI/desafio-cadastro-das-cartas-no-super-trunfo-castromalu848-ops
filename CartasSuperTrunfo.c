f12eba30ddf82013de398d70117b9e581fa8b331
castromalu848-ops <castromalu848@gmail.com>
2025-09-02 21:29:23 +0000

    Desafio Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,12 +3,14 @@
 int main() {
     
     char nome1 [50];
-    char estado1 [3];
-    char codigo1 [50];
+    char estado1 [3] ;
+    char codigo1 [5];
     int numero_de_pontos_turisticos1;
     int populacao1;
     float PIB1;
     float area1;
+    float densidade1;
+    float PIB_capita1;
 
     char nome2 [50];
     char estado2[3];
@@ -17,6 +19,8 @@ int main() {
     int populacao2;
     float PIB2;
     float area2;
+    float densidade2;
+    float PIB_capita2;
 
     // Carta 1 
 
@@ -43,12 +47,16 @@ int main() {
     printf( "Digite o número da area 1: \n");
     scanf("%f", &area1);
 
+    densidade1 = populacao1 / area1;
+    PIB_capita1 = PIB1 / populacao1;
+
+
     // Carta 2 
 
     printf("\n =====Dados da carta 2===== \n\n");
 
     printf( "Digite o nome da cidade 2: \n");
-    scanf(" %49[^\n]",nome2);
+    scanf(" %49[^\n]", nome2);
 
     printf( "Digite a sigla do Estado 2: \n");
     scanf("%s", estado2);
@@ -68,8 +76,11 @@ int main() {
     printf( "Digite o número da area 2: \n");
     scanf("%f", &area2);
 
-    printf("\n  ======Dados da carta 1===== \n"" Nome da cidade: %s \n estado: %s \n codigo: %s \n numero de pontos turisticos: %d \n populacao: %d \n PIB: %.2f \n Area: %.2f", nome1, estado1, codigo1, numero_de_pontos_turisticos1, populacao1, PIB1, area1);                     
-    printf("\n\n =====Dados da carta 2===== \n"" Nome da cidade: %s \n estado: %s \n codigo: %s \n numero de pontos turisticos: %d \n populacao: %d \n PIB: %.2f \n Area: %.2f", nome2, estado2, codigo2, numero_de_pontos_turisticos2, populacao2, PIB2, area2);                   
+    densidade2 = populacao2 / area2;
+    PIB_capita2 = PIB2 / populacao2;
+
+    printf("\n  ======Dados da carta 1===== \n"" Nome da cidade: %s \n estado: %s \n codigo: %s \n numero de pontos turisticos: %d \n populacao: %d \n PIB: %.2f \n Area: %.2f \n densidade: %.2f \n PIB_capita: %.2f", nome1, estado1, codigo1, numero_de_pontos_turisticos1, populacao1, PIB1, area1, densidade1, PIB_capita1);                     
+    printf("\n\n =====Dados da carta 2===== \n"" Nome da cidade: %s \n estado: %s \n codigo: %s \n numero de pontos turisticos: %d \n populacao: %d \n PIB: %.2f \n Area: %.2f \n densidade: %.2f \n PIB_capita: %.2f", nome2, estado2, codigo2, numero_de_pontos_turisticos2, populacao2, PIB2, area2, densidade2, PIB_capita2);                   
     
     return 0;
 }
